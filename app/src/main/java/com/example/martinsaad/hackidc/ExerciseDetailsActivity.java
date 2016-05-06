2947b13dc6a18b510b0ee609fa5a676b29c38323
Adi Azarya <adiazarya100@gmail.com>
2016-05-06 21:25:59 +0300

    new workout screen, with swap option+ last exe values

--- app/src/main/java/com/example/martinsaad/hackidc/ExerciseDetailsActivity.java
+++ app/src/main/java/com/example/martinsaad/hackidc/ExerciseDetailsActivity.java
@@ -1,7 +1,11 @@
 package com.example.martinsaad.hackidc;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ExerciseDetailsActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class ExerciseDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise_details);
+
+        ImageButton btn = (ImageButton) findViewById(R.id.button_swap_excerise);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("IDC", "adi ya zain");
+            }
+        });
     }
 }
