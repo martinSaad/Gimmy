a67c5656a1cab71afc2f4e13def1cd18e4f90f9a
Rea Bar <Rea.Bar@gmail.com>
2016-05-06 22:36:07 +0300

    working on register fragment

--- app/src/main/java/com/example/martinsaad/hackidc/RegisterFragment.java
+++ app/src/main/java/com/example/martinsaad/hackidc/RegisterFragment.java
@@ -4,9 +4,20 @@ package com.example.martinsaad.hackidc;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RegisterFragment extends Fragment {
 
@@ -21,6 +32,46 @@ public class RegisterFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         fragmentCommunicator = (FragmentCommunicator) getActivity();
+
+        Button registerButton = (Button) getActivity().findViewById(R.id.button_Register_buttonRegister);
+        final EditText firstName = (EditText) getActivity().findViewById(R.id.editText_Register_firstName);
+        final EditText lastName = (EditText) getActivity().findViewById(R.id.editText_Register_lastName);
+        final DatePicker birthDate = (DatePicker) getActivity().findViewById(R.id.datePicker_Register_birthDay);
+        final Switch gender = (Switch) getActivity().findViewById(R.id.switch_Register_gender);
+        final EditText email = (EditText) getActivity().findViewById(R.id.editText_Register_email);
+        final EditText password = (EditText) getActivity().findViewById(R.id.editText_Register_password);
+        final EditText repeatPassword = (EditText) getActivity().findViewById(R.id.editText_Register_repeatPassword);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String pass = password.getText().toString();
+                String repeatPass = repeatPassword.getText().toString();
+
+                if (firstName!=null && lastName!=null && email!=null && password!=null && repeatPassword!=null){
+                    if (!pass.equals(repeatPass)){
+                        Toast.makeText(getActivity(), "passwords not the same", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        Toast.makeText(getActivity(), "not all fields are full", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                String gend = gender.isChecked() ? "Male" : "Female";
+                User user = new User(firstName.getText().toString(), lastName.getText().toString(), email.getText().toString(), pass, gend, birthDate.getText().toString());
+
+                Gson gson = new Gson();
+                String json = gson.toJson(user);
+                List<String> parameters = new ArrayList<String>();
+                parameters.add(Constants.TRAINEES);
+                try {
+                    httpRequest.doPost(json, parameters);
+                }catch (IOException e){
+                    Log.d("error", e.toString());
+                }
+
+            }
+        });
     }
 
 }
