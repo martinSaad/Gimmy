24c03471105b30cfd0c8220bac55363f68888dc7
Rea Bar <Rea.Bar@gmail.com>
2016-05-06 14:31:35 +0300

    working on Register Activity

--- app/src/main/java/com/example/martinsaad/hackidc/MainActivity.java
+++ app/src/main/java/com/example/martinsaad/hackidc/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.martinsaad.hackidc;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -17,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Intent registerActivity = new Intent(this, RegisterActivity.class);
+        startActivity(registerActivity);
     }
 
     @Override
---
+++ app/src/main/java/com/example/martinsaad/hackidc/RegisterActivity.java
@@ -0,0 +1,35 @@
+package com.example.martinsaad.hackidc;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Button registerButton = (Button) findViewById(R.id.buttonRegister);
+        final EditText firstName = (EditText) findViewById(R.id.editText_firstName);
+        final EditText lastName = (EditText) findViewById(R.id.editText_lastName);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("MARTIN", firstName.getText().toString());
+                Log.d("MARTIN", lastName.getText().toString());
+            }
+        });
+    }
+
+}
