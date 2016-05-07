4335a41c82be4287e6a49a1fbbf119c88df22cd7
Adi Azarya <adiazarya100@gmail.com>
2016-05-07 14:08:27 +0300

    design

--- app/src/main/java/com/example/martinsaad/hackidc/MainActivity.java
+++ app/src/main/java/com/example/martinsaad/hackidc/MainActivity.java
@@ -41,8 +41,7 @@ public class MainActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
 
         drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
