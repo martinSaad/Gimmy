7df55a95b3ff73cce4f065eb97f1fe3fa7c59fea
Rea Bar <Rea.Bar@gmail.com>
2016-05-07 10:37:51 +0300

    WORKING LOGIN VALIDATION

--- app/src/main/java/com/example/martinsaad/hackidc/PerformanceFragment.java
+++ app/src/main/java/com/example/martinsaad/hackidc/PerformanceFragment.java
@@ -6,17 +6,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.Viewport;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class PerformanceFragment extends Fragment {
     FragmentCommunicator fragmentCommunicator;
     private static final Random RANDOM = new Random();
-    private LineGraphSeries<DataPoint> series;
     private int lastX = 0;
 
     @Override
@@ -30,23 +35,75 @@ public class PerformanceFragment extends Fragment {
         fragmentCommunicator = (FragmentCommunicator) getActivity();
         fragmentCommunicator.passString("enableDrawer");
 
+        List<String> params = new ArrayList<>();
+        params.add("trainees");
+        params.add("5");
+        params.add("get_average_of_exercise_weight_by_date");
+
+        Request r = new Request("GET", params, null);
+        new HttpRequest(new AsyncResponse() {
+            @Override
+            public void processFinish(String output) {
+                if (output==null){
+                }
+                else {
+                    try {
+                        JSONArray jsonArray = new JSONArray(output);
+                        GraphView graph = (GraphView) getActivity().findViewById(R.id.graph);
+                        DataPoint[] dataPoints = new DataPoint[jsonArray.length()];
+                        // data
+                        for(int i = 0 ; i < jsonArray.length() ; i++) {
+                            JSONObject jsonObj = jsonArray.getJSONObject(i);
+                            int avg = (int) jsonObj.get("average");
+                            dataPoints[i] = new DataPoint(i, avg);
+                        }
+                        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(dataPoints);
+                        graph.addSeries(series);
+                        Viewport viewport = graph.getViewport();
+                        viewport.setYAxisBoundsManual(true);
+                        viewport.setMinY(0);
+                        viewport.setMaxY(10);
+                        viewport.setScrollable(true);
+
+                    } catch (JSONException e){
+
+                    }
+                }
+            }
+/*
+                }
+            }
+        }).execute(r, null, null);
+
+
         // we get graph view instance
         GraphView graph = (GraphView) getActivity().findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[jsonArray.length()]);
+        DataPoint[] dataPoints = new DataPoint[jsonArray.length()];
         // data
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+        for(int i = 0 ; i < jsonArray.length() ; i++){
+            try {
+                JSONObject tempObj = (JSONObject) jsonArray.get(i);
+                dataPoints[i] = new DataPoint(i, tempObj.getInt("average"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+/*        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
                 new DataPoint(0, 1),
                 new DataPoint(1, 5),
                 new DataPoint(2, 3),
                 new DataPoint(3, 2),
                 new DataPoint(4, 6)
         });
-        graph.addSeries(series);
-        // customize a little bit viewport
-        Viewport viewport = graph.getViewport();
+        graph.addSeries(series);*/
+            // customize a little bit viewport
+/*                Viewport viewport = graph.getViewport();
         viewport.setYAxisBoundsManual(true);
         viewport.setMinY(0);
         viewport.setMaxY(10);
-        viewport.setScrollable(true);
+        viewport.setScrollable(true);*/
+        });
     }
-
 }
+
